3121479500663711d1fb050aa8acb7356cf7f239
Carmela <carmela881@outlook.com>
2023-04-24 09:41:19 +0100

    added function to add roles

---
+++ validate.js
@@ -0,0 +1,18 @@
+const validator = require('validator');
+
+const validate = {
+  validateString(str) {
+    return str !== '' || 'Please enter a valid response!';
+  },
+  validateSalary(num) {
+    if (validator.isDecimal(num)) {
+      return true;
+    }
+    return 'Please enter a valid decimal number!';
+  },
+  isSame(str1, str2) {
+    if (str1 === str2) return true;
+  }
+};
+
+module.exports = validate;
